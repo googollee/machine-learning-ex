e6af0ee0f6b3c235bd964163cc9c95495d0701e6
Googol Lee <i@googol.im>
2016-11-19 12:02:36 +0100

    update directory structure

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,10 +20,12 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+p1 = - y * (log(sigmoid(X * theta)))';
+p2 = - (1-y) * (log(1 - sigmoid(X * theta)))';
+J = (p1 + p2) ./ m;
 
 
-
-
+grad = X' * (sigmoid(X * theta) - y) ./ m;
 
 
 
